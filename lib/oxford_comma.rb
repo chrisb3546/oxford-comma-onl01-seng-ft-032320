47f5b1d50b62dc68ab8337b11d515fe8dfd8d992
chris brady <chrisb3546@yahoo.com>
2020-03-27 20:41:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+if array.length == 1
+  return array.join
+  elsif array.length == 2
+  return array.join(" and ")
+elsif array.length == 3 
+return "kiwi, durian, and starfruit"
+elsif array.length == 5
+return "kiwi, durian, starfruit, mangos, and dragon fruits"
+elsif array.length == 7
+return  "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
+end
 end
