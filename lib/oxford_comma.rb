0cee737617f183df2993e63d9e68e44eadf9ffde
chris brady <chrisb3546@yahoo.com>
2020-03-27 20:27:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ if array.length == 1
   elsif array.length == 2
   return array.join(" and ")
 elsif array.length == 3 
-array.pop
+array.pop = sf
 array = array.join(", ")
-return array.push(array.pop)
+return array.push(sf)
 end
 end
