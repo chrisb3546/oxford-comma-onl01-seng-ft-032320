09bba6bf0212d4346d7ed1c76d00422c45964bfc
chris brady <chrisb3546@yahoo.com>
2020-03-27 20:11:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length == 1
   return array.join
   elsif array.length == 2
   return array.join(" and ")
-elsif array.length == 3 array.pop, 
-return 
+elsif array.length == 3  
+return array.to_sentence
 end
 end
