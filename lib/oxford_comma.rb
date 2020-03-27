a46d86c8633974a248faea9399994fdce2d111fd
chris brady <chrisb3546@yahoo.com>
2020-03-27 20:31:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ if array.length == 1
   elsif array.length == 2
   return array.join(" and ")
 elsif array.length == 3 
-sf = array.pop 
+sf = " and starfruit" 
 array = array.join(", ")
-return array + 
+return array #{sf}
 end
 end
